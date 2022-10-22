a6ec55e51ea07eab436be161d36f51ade9bac14a
pavel dolgushin <pfdolgushin@gmail.com>
2022-10-22 17:00:17 +0500

    Исправление имени метода test_modificate_first_group

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-def test_delete_first_group(app):
+def test_modificate_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modificate_first_group(Group(name="modificate_name", header="modificate_header", footer="modificate_footer"))
     app.session.logout()
