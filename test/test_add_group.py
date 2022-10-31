e9c4b4688845b35aed70a9fa9db2273543589ab6
pavel dolgushin <pfdolgushin@gmail.com>
2022-10-31 08:47:05 +0500

    Сортировка списка групп в тестах добавления, модификации групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,9 +3,18 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="new_2", header="header_2", footer="footer_2"))
+    group = Group(name="new_2", header="header_2", footer="footer_2")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert (len(old_groups)+1) == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))
+    old_groups = app.group.get_group_list()
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert (len(old_groups)+1) == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -5,10 +5,13 @@ def test_modificate_first_group(app):
     if app.count('group') == 0:
         app.group.create(Group(name='name_group_for_modify'))
     old_groups = app.group.get_group_list()
-    app.group.modificate_first_group(Group(name="modificate_name", header="modificate_header", footer="modificate_footer"))
+    group = Group(name="modificate_name", header="modificate_header", footer="modificate_footer")
+    group.id = old_groups[0].id
+    app.group.modificate_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_modificate_name_in_first_group(app):
     if app.count('group') == 0:
