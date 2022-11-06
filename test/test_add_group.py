57246456bb6caab62364ec745f4a440eed2e603c
pavel dolgushin <pfdolgushin@gmail.com>
2022-11-06 20:16:28 +0500

    Используем метод app.count('group') как хэш для списка групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,8 +5,8 @@ def test_add_group(app):
     old_groups = app.group.get_group_list()  # список групп до проведения шагов
     group = Group(name="new_2", header="header_2", footer="footer_2")
     app.group.create(group)  # создали группу
-    new_groups = app.group.get_group_list()   # список групп после проведения шагов
-    assert (len(old_groups)+1) == len(new_groups)   # кол-во групп до теста + 1 = кол-ву групп после теста
+    assert (len(old_groups)+1) == app.count('group')   # кол-во групп до теста + 1 = кол-ву групп после теста
+    new_groups = app.group.get_group_list()  # список групп после проведения шагов
     old_groups.append(group)  # к списку групп до теста добавили группу которую создавали и сравним со списком после теста
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
@@ -14,7 +14,7 @@ def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="", header="", footer="")
     app.group.create(group)
+    assert (len(old_groups)+1) == app.count('group')    # метод app.count('group') в роли хэш от get_group_list()
     new_groups = app.group.get_group_list()
-    assert (len(old_groups)+1) == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
