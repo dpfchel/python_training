62e9b2524a8a9c575887d22fb7598e44819cac2d
pavel dolgushin <pfdolgushin@gmail.com>
2022-11-27 19:08:18 +0500

    В тесте test_add_group список групп берем из БД

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,22 +9,29 @@ from fixture.application import Application as appl
 
 #@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 
-def test_add_group(app, json_dataset_group):
+def test_add_group(app, db, json_dataset_group):
     group = json_dataset_group
-    old_groups = app.group.get_group_list()  # список групп до проведения шагов
+    old_groups = db.get_group_list()  # список групп до проведения шагов
     app.group.create(group)  # создали группу
-    assert (len(old_groups)+1) == app.count('group')   # кол-во групп до теста + 1 = кол-ву групп после теста
-    new_groups = app.group.get_group_list()  # список групп после проведения шагов
+    #assert (len(old_groups)+1) == app.count('group')   # кол-во групп до теста + 1 = кол-ву групп после теста
+    new_groups = db.get_group_list()  # список групп после проведения шагов
     old_groups.append(group)  # к списку групп до теста добавили группу которую создавали и сравним со списком после теста
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_group_groupname_have_space_in_border(app):
-    group = Group(name="     name     ", header="   header    ", footer="    footer    ")
-    old_groups = app.group.get_group_list()  # список групп до проведения шагов
+def test_add_group_groupname_have_space_in_border(app, db):
+    group = Group(name="     name1     ", header="   header1    ", footer="    footer1    ")
+    #old_groups = app.group.get_group_list()  # список групп до проведения шагов
+    old_groups = db.get_group_list()
     app.group.create(group)  # создали группу
     assert (len(old_groups)+1) == app.count('group')   # кол-во групп до теста + 1 = кол-ву групп после теста
-    new_groups = app.group.get_group_list()  # список групп после проведения шагов
-    group.name = (group.name)[5:-5]
+    #new_groups = app.group.get_group_list()  # список групп после проведения шагов
+    new_groups = db.get_group_list()
+    #group.name = (group.name)[5:-5] # для проверкок через UI - пробелы удаляются
+    group.name = (group.name)  # для проверкок через DB - пробелы не удаляются
     old_groups.append(group)  # к списку групп до теста добавили группу которую создавали и сравним со списком после теста
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    
+
+#из набора данных удалил пробелы в середине слов.
+# При работе с БД из набора данных удалил двойной обратный слеш  \\
