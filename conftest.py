a01937f40b3432206a0cf4e631f3801ed85cb616
pavel dolgushin <pfdolgushin@gmail.com>
2022-11-27 19:37:28 +0500

    В тесте test_del_group сделали отключаемыми проверки UI по check_ui

--- conftest.py
+++ conftest.py
@@ -55,6 +55,7 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='chrome')
     parser.addoption('--target', action='store', default='target.json') #'target.json'
+    parser.addoption('--check_ui', action='store_true') #store_true - автоматом True если опция есть, False если отсутствует
 
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
@@ -71,3 +72,7 @@ def load_from_module(module):
 def load_from_json(file):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
         return jsonpickle.decode(f.read())
+
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
