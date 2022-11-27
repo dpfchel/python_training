3850758de7584814bce51c2a4d21febfb03924b8
pavel dolgushin <pfdolgushin@gmail.com>
2022-11-27 21:21:13 +0500

    В тесте test_add_contacts получаем списки из БД. Проверки UI - ключ check_ui

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,7 +9,8 @@ from fixture.application import Application as appl
 
 #@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 
-def test_add_group(app, db, json_dataset_group):
+# Получение списка контактов из DB, отключаемая проверка UI
+def test_add_group(app, db, json_dataset_group, check_ui):
     group = json_dataset_group
     old_groups = db.get_group_list()  # список групп до проведения шагов
     app.group.create(group)  # создали группу
@@ -17,8 +18,11 @@ def test_add_group(app, db, json_dataset_group):
     new_groups = db.get_group_list()  # список групп после проведения шагов
     old_groups.append(group)  # к списку групп до теста добавили группу которую создавали и сравним со списком после теста
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(app.group.get_group_list(), key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
+"""
 def test_add_group_groupname_have_space_in_border(app, db):
     group = Group(name="     name1     ", header="   header1    ", footer="    footer1    ")
     #old_groups = app.group.get_group_list()  # список групп до проведения шагов
@@ -33,6 +37,6 @@ def test_add_group_groupname_have_space_in_border(app, db):
     old_groups.append(group)  # к списку групп до теста добавили группу которую создавали и сравним со списком после теста
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     
-
+"""
 #из набора данных удалил пробелы в середине слов.
 # При работе с БД из набора данных удалил двойной обратный слеш  \\
