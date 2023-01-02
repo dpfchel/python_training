636bbf0892146575d60c6650e63aed54d53f8dc9
pavel dolgushin <pfdolgushin@gmail.com>
2023-01-02 15:41:10 +0500

    Доработка теста 21

--- fixture/db.py
+++ fixture/db.py
@@ -22,15 +22,35 @@ class DbFixture:
         finally:
             cursor.close()
         return list
-
+# contact.home_telephone, contact.mobile_telephone, contact.work_telephone, contact.home_phone2
     def get_contact_list(self):
         cursor = self.connection.cursor()
         list = []
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            sql_my = "select " \
+                     "id, " \
+                     "firstname, " \
+                     "lastname, " \
+                     "address, " \
+                     "email, email2, email3, " \
+                     "home, mobile, work, phone2 " \
+                     "from addressbook " \
+                     "where deprecated='0000-00-00 00:00:00'"
+            cursor.execute(sql_my)
             for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, address, email, email2, email3, home, mobile, work, phone2) = row
+                list.append(Contact(id=str(id), firstname=firstname.strip(), lastname=lastname.strip(),
+                                    address=address.strip(), email=email.strip(), email2=email2.strip(),
+                                    email3=email3.strip(),
+                                    home_telephone=home,
+                                    mobile_telephone=mobile,
+                                    work_telephone=work,
+                                    home_phone2=phone2
+                                    #home_telephone=home.strip().replace('/', '').replace('.', ''),  # Решил убирать лишние символы в самом тесте
+                                    #mobile_telephone=mobile.strip().replace('/', '').replace('.', ''),  #убираю из телефонов последние пробелы, слеш, точку, для сравнения с выводимым на экран
+                                    #work_telephone=work.strip().replace('/', '').replace('.', ''),
+                                    #home_phone2=phone2.strip().replace('/', '').replace('.', '')
+                                    ))
         finally:
             cursor.close()
         return list
--- test/test_check_contact_homepage_vs_db_21.py
+++ test/test_check_contact_homepage_vs_db_21.py
@@ -1,8 +1,38 @@
+import re
 from fixture.contact import Contact
 
 def test_phones_on_home_page(app, db):
+    with open('c:/temp/21_list_contacts_home_i.firstname.txt', 'w') as f:
+        f.write('')
+    with open('c:/temp/21_list_contacts_db_i.firstname.txt', 'w') as f:
+        f.write('')
+
     list_contacts_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
     list_contacts_home = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
     assert len(list_contacts_db) == len(list_contacts_home)
     for i in range(len(list_contacts_db)):
-        assert list_contacts_db[i] == list_contacts_home[i]
+        list_contacts_home_i = list_contacts_home[i]
+        list_contacts_db_i = list_contacts_db[i]
+        assert list_contacts_home_i.firstname == list_contacts_db_i.firstname
+        assert list_contacts_home_i.lastname == list_contacts_db_i.lastname
+        assert list_contacts_home_i.address == list_contacts_db_i.address
+        assert list_contacts_home_i.all_email_from_home_page == merge_email_like_on_home_page(list_contacts_db_i)
+        assert list_contacts_home_i.all_phones_from_home_page == merge_phones_like_on_home_page(list_contacts_db_i)
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter( lambda x: x is not None,
+                                        #[contact.home_telephone, contact.mobile_telephone, contact.work_telephone, contact.home_phone2]))))
+                                        [contact.home_telephone.strip().replace('/', '').replace('.', ''),
+                                         contact.mobile_telephone.strip().replace('/', '').replace('.', ''),
+                                         contact.work_telephone.strip().replace('/', '').replace('.', ''),
+                                         contact.home_phone2.strip().replace('/', '').replace('.', '')]))))
+
+def merge_email_like_on_home_page(contact):
+    return "\n".join(filter (lambda x: x != "",
+                             filter ( lambda x: x is not None,
+                                      [contact.email, contact.email2, contact.email3])))
