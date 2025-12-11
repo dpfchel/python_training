4ee725992b41b843cc47de8c25d5908174f36c15
pavel dolgushin <pfdolgushin@gmail.com>
2025-12-11 10:03:26 +0500

    add check_db_connection.py

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,6 +1,6 @@
 from fixture.db import DbFixture
 
-db = DbFixture(host='127.0.0.1', database='addressbook', user='root', password='')
+db = DbFixture(host='127.0.0.1', name='addressbook', user='root', password='')
 
 try:
     groups = db.get_group_list()
